cf24d875ae2242228bb26f2f7c1eb8cdaba2b02b
Varun Aggarwal <varunaggarwal1995@gmail.com>
2017-06-22 21:49:42 +0530

    Update Assi3.java

--- assi3/src/assi3/Assi3.java
+++ assi3/src/assi3/Assi3.java
@@ -23,26 +23,14 @@ class Student {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public double getCgpa() {
         return cgpa;
     }
-
-    public void setCgpa(double cgpa) {
-        this.cgpa = cgpa;
-    }
-
+    
 }
 
 public class Assi3 {
